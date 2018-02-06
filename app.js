0e7cd3da6155828877c56082e720626295b1df9c
owenliversidge <owen.liversidge@gmail.com>
2018-02-06 15:19:31 -0500

    added bootstrap functionality

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //assign letters as 
+
 window.onload = function() {
     let a = document.querySelector('#a');
     let b = document.querySelector('#b');
@@ -26,6 +27,7 @@ window.onload = function() {
     let y = document.querySelector('#y');
     let z = document.querySelector('#z');
 }
+
 //Pseudocode for the game "Trumpisms"
 
 //an array of words to be used
@@ -48,7 +50,7 @@ let remainingLetters = randomWord.length;
 
 //if there are remining letters
 while (remainingLetters > 0) {
-    ("This is how many letters remain in my super smart word:\n" + answerArray.join(" "));
+    alert("This is how many letters remain in my super smart word:\n" + answerArray.join(" "));
 
     //game begins
     //ask player to input a letter
