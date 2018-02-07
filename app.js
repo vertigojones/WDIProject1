e3c5b4d2f0e4bab28cdb4641f69ce228a98fae6b
owenliversidge <owen.liversidge@gmail.com>
2018-02-06 19:52:56 -0500

    jQuery DOM working

--- app.js
+++ app.js
@@ -21,32 +21,42 @@ let remainingLetters = randomWord.length;
 
 //if there are remining letters
 //while (remainingLetters > 0) {
-    $(`#input`).append("These are the letters that remain in my super smart word:\n" + answerArray.join(" "));
+$(`#input`).text("These are the letters that remain in my super smart word:\n" + answerArray.join(" "));
 
-    //game begins
-    //ask player to input a letter
-    //let guess = prompt("I have a very good brain. Bet you can't guess one of the smart words I have used in the past! Enter a letter and find out.");
-    $(`.button`).click(function (guess) {
-        let value = guess.target.innerText
-        console.log(value)
-        //player enters no letters
-        //if (prompt === null) {
-        //end game
-        //break;
-        //player enters too many letters
-        //} else if (guess.length !== 1) {
-        //    alert("Please enter one single letter. Dummy!");
-        //} else {
-        //Update match with guess
-        for (let j = 0; j < randomWord.length; j++) {
-            if (randomWord[j] === value) {
-                answerArray[j] = value;
-                remainingLetters--;
-            }
+//game begins
+//ask player to input a letter
+//let guess = prompt("I have a very good brain. Bet you can't guess one of the smart words I have used in the past! Enter a letter and find out.");
+$(`.button`).click(function (guess) {
+    let value = guess.target.innerText
+    console.log(value)
+    //player enters no letters
+    //if (prompt === null) {
+    //end game
+    //break;
+    //player enters too many letters
+    //} else if (guess.length !== 1) {
+    //    alert("Please enter one single letter. Dummy!");
+    //} else {
+    //Update match with guess
+    for (let j = 0; j <= randomWord.length; j++) {
+
+        if (j == randomWord.length) {
+            $(`#comments`).text("If only you had an IQ as high as me. Dummy. Try again!")
         }
-        //}
-    })
-//}
+
+        else if (randomWord[j] === value) {
+            answerArray[j] = value;
+            $(`#input`).text("These are the letters that remain in my super smart word:\n" + answerArray.join(" "));
+            $(`#comments`).text("Bingo!")
+            remainingLetters--;
+            break;
+        }
+
+
+    }
+    //}
+})
+
 console.log(answerArray.join(" "))
 //$(`#input`).append("You're hired! You guessed that my super-duper smart word is " + randomWord + "!");
 /*
