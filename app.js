ed821aeec08de618855ffb81216c3660716ae207
owenliversidge <owen.liversidge@gmail.com>
2018-02-06 21:55:11 -0500

    fixed some css

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 
-//Pseudocode for the game "Trumpisms"
-
-//an array of words to be used
 const words = ['COVFEFE', 'BIGLY', 'OMBRE', 'NAMBIA', 'MISOGYNY', 'COMPLICIT', 'BRAGGADOCIO'];
 //randomise a word from array
 let randomWord = words[Math.floor(Math.random() * words.length)];
-
-//turn word to uppercase 
 
 //turn the word into a series of individual characters
 let answerArray = [];
@@ -19,31 +14,20 @@ for (let i = 0; i < randomWord.length; i++) {
 //check for remaining letters
 let remainingLetters = randomWord.length;
 
-//if there are remining letters
-//while (remainingLetters > 0) {
+//reveal array in browser
 $(`#input`).text("These are the letters that remain in my super smart word:\n" + answerArray.join(" "));
 
 //game begins
-//ask player to input a letter
-//let guess = prompt("I have a very good brain. Bet you can't guess one of the smart words I have used in the past! Enter a letter and find out.");
+//player guesses a letter by clicking letter button
 $(`.button`).click(function (guess) {
     let value = guess.target.innerText
-    console.log(value)
-    //player enters no letters
-    //if (prompt === null) {
-    //end game
-    //break;
-    //player enters too many letters
-    //} else if (guess.length !== 1) {
-    //    alert("Please enter one single letter. Dummy!");
-    //} else {
-    //Update match with guess
+    //check through letters of word
     for (let j = 0; j <= randomWord.length; j++) {
-
+        //wrong guess, return insult
         if (j == randomWord.length) {
             $(`#comments`).text("If only you had an IQ as high as me. Dummy. Try again!")
         }
-
+        //correct guess, return compliment and reveal letters in srray
         else if (randomWord[j] === value) {
             answerArray[j] = value;
             $(`#input`).text("These are the letters that remain in my super smart word:\n" + answerArray.join(" "));
@@ -51,17 +35,5 @@ $(`.button`).click(function (guess) {
             remainingLetters--;
             break;
         }
-
-
     }
-    //}
 })
-
-console.log(answerArray.join(" "))
-//$(`#input`).append("You're hired! You guessed that my super-duper smart word is " + randomWord + "!");
-/*
-$(`.button`).click((target)=> {
-    let value = target.target.id
-    
-})
-*/
