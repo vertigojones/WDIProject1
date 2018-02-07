5596c0a159a289cc0804847600a820d7857a8012
owenliversidge <owen.liversidge@gmail.com>
2018-02-07 11:15:29 -0500

    fixed multiple letters correct

--- app.js
+++ app.js
@@ -6,16 +6,12 @@ let randomWord = words[Math.floor(Math.random() * words.length)];
 //comments to be returned for wrong guesses
 const responses = ["If only you had an IQ as high as me. Dummy. Try again!", "So sad!", "Do you come from a s**thole or something?", "LOSER!!!", "Horrible job!", "Did you graduate last in your class?", "Hokey garbage...", "What a dope!"];
 //randomise a word from array
-//let selectResponse = responses[Math.floor(Math.random() * responses.length)];
-    //let randomResponse = [];
-    //for (let i = 0; i )
-
-
+//selectResponse = responses[Math.floor(Math.random() * responses.length)];
+    
 //turn the word into a series of individual characters
 let answerArray = [];
 for (let i = 0; i < randomWord.length; i++) {
     //turn the characters into underscores
-    randomWord.split('');
     answerArray[i] = "_";
 }
 
@@ -32,17 +28,16 @@ $(`.button`).click(function (guess) {
     let value = guess.target.innerText;
     //check through letters of word
     for (let j = 0; j <= randomWord.length; j++) {
-        //wrong guess, return insult
-        if (j == randomWord.length) {
-            //$(`#comments`).delay(800).text(randomResponse);
-        }
         //correct guess, return compliment and reveal letters in array
-        else if (randomWord[j] === value) {
+        if (randomWord[j] === value) {
             answerArray[j] = value;
             $(`#input`).text("These are the letters that remain in my super smart word:\n" + answerArray.join(" "));
             $(`#comments`).delay(800).text("Bingo!");
             remainingLetters--;
-            break;
         }
+        //wrong guess, return insult
+        else if (j == randomWord.length) {
+            //$(`#comments`).delay(800).text([randomNumber]);
+        }       
     }
 })
