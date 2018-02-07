650de56835be3e872b84fb10da3c64c6b6d9cde2
owenliversidge <owen.liversidge@gmail.com>
2018-02-07 12:00:48 -0500

    fixed insult return

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const words = ['COVFEFE', 'BIGLY', 'OMBRE', 'NAMBIA', 'MISOGYNY', 'COMPLICIT', '
 //randomise a word from array
 let randomWord = words[Math.floor(Math.random() * words.length)];
 
-//comments to be returned for wrong guesses
+//compliments to be returned for correct answer
+const compliments = ["You have a smart brain!", "You should work for Fox News!", "Great going!", "Very powerful, very strong. Keep going!", "Just superb!", "Are you from Russia"]
+
+//insults to be returned for wrong guesses
 const responses = ["If only you had an IQ as high as me. Dummy. Try again!", "So sad!", "Do you come from a s**thole or something?", "LOSER!!!", "Horrible job!", "Did you graduate last in your class?", "Hokey garbage...", "What a dope!"];
-//randomise a word from array
-//selectResponse = responses[Math.floor(Math.random() * responses.length)];
-    
+
 //turn the word into a series of individual characters
 let answerArray = [];
 for (let i = 0; i < randomWord.length; i++) {
@@ -28,16 +29,16 @@ $(`.button`).click(function (guess) {
     let value = guess.target.innerText;
     //check through letters of word
     for (let j = 0; j <= randomWord.length; j++) {
-        //correct guess, return compliment and reveal letters in array
+        //correct guess, return random compliment and reveal letters in array
         if (randomWord[j] === value) {
             answerArray[j] = value;
             $(`#input`).text("These are the letters that remain in my super smart word:\n" + answerArray.join(" "));
-            $(`#comments`).delay(800).text("Bingo!");
+            $(`#comments`).delay(800).text(compliments[Math.floor(Math.random() * compliments.length)]);
             remainingLetters--;
         }
-        //wrong guess, return insult
+        //wrong guess, return random insult
         else if (j == randomWord.length) {
-            //$(`#comments`).delay(800).text([randomNumber]);
+                $(`#comments`).delay(800).text(responses[Math.floor(Math.random() * responses.length)]);
         }       
     }
 })
